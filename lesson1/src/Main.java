e193f85405fc43dfeb436a7a5cc2f8e88143f044
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-05-10 16:50:20 +0500

    Sample testing file

---
+++ lesson1/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static  void main(String[] args){
+        int number = 123;
+        byte isByte = 100;
+        short isShort = 852;
+        long isLong = 233223423423L;
+
+        double isDouble = 456.45;
+        float isFloat = 234.32F;
+
+        boolean isTrue = true;
+        boolean isFalse = false;
+
+        char isChar = 'a';
+
+        System.out.println("Hello World");
+    }
+}
