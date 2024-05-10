8f7eecc7e16fc473b6e763010fa78c6d5846c787
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-05-10 16:50:31 +0500

    Data type files

---
+++ lesson1/src/DataTypes.java
@@ -0,0 +1,19 @@
+public class DataTypes {
+    public  static void  main(String[] args){
+        int number = 123;
+        byte isByte = 100;
+        short isShort = 852;
+        long isLong = 233223423423L;
+
+        double isDouble = 456.45;
+        float isFloat = 234.32F;
+
+        boolean isTrue = true;
+        boolean isFalse = false;
+
+        char isChar = 'a';
+
+        System.out.println("printing the char '" + isChar+ "'");
+
+    }
+}
