eb4ce06fd9f68a47e030d396374f3cdfe5bdb87a
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-05-10 16:50:42 +0500

    Operators files

---
+++ lesson1/src/Operators.java
@@ -0,0 +1,18 @@
+public class Operators {
+    public  static  void  main(String[] args){
+        int add = 5+1;
+        System.out.println("adding numbers using operator '+' "+ add);
+        int sub = 5-3;
+        System.out.println("subtracting number using operator '-' " +sub);
+        int num1 = 12;
+        int num2 = 6;
+        int total = 12/6;
+        System.out.println("division int numbers "+ total);
+        double totalDoubleDivisor = 12/5.5;
+        System.out.println("division int numbers "+ totalDoubleDivisor);
+        double totalDoubleDividend = 12.5/5;
+        System.out.println("division int numbers "+ totalDoubleDividend);
+        int remainder = 12%5;
+        System.out.println("Finding Remainder with % operator "+ remainder);
+    }
+}
