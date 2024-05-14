21e057b39e5aa9d062f96e2eb56aa0afd0478aa3
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-05-14 12:57:54 +0500

    Worked on user Inputs

--- lesson1/src/UserInputs.java
+++ lesson1/src/UserInputs.java
@@ -1,8 +1,16 @@
 import java.util.Scanner;
 
 public class UserInputs {
-    public  static  void main(){
-        Scanner userInput = new Scanner(System.in);
+    public  static  void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String userInput = scanner.nextLine();
+        System.out.print("What is your age? ");
+        int age = Integer.parseInt(scanner.nextLine());
+        System.out.print("What programming language you are learning right now? ");
+        String programingLanguage = scanner.nextLine();
+
+        System.out.printf("Hi I am %s, I am %d years old. And right now I am learning %s",userInput,age,programingLanguage);
 
 
     }
