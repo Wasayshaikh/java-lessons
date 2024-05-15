8d5485e0b33febdb2cc2241cbaeccbda9aa576ad
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-05-15 20:55:07 +0500

    Working on creating new HashMap variable

--- lesson1/src/HashMapLesson.java
+++ lesson1/src/HashMapLesson.java
@@ -2,7 +2,14 @@ import java.util.HashMap;
 
 public class HashMapLesson {
     public  static void main(String[] args){
+        HashMap<String, Integer> examResult = new HashMap<String, Integer>();
+        examResult.put("Math", 75);
+        examResult.put("Chemistry", 85);
+        examResult.put("Computer Programming", 92);
+        examResult.put("Physics",85);
 
+        System.out.println(examResult.toString());
+        
 
     }
 }
