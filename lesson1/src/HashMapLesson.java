a8796d18e35832dbea0333523f8bd68ac7d748f4
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-05-16 19:22:33 +0500

    Working with HashMap

--- lesson1/src/HashMapLesson.java
+++ lesson1/src/HashMapLesson.java
@@ -9,7 +9,55 @@ public class HashMapLesson {
         examResult.put("Physics",85);
 
         System.out.println(examResult.toString());
-        
+
+        System.out.println("Replace Value");
+        examResult.replace("Chemistry",80);
+        System.out.println(examResult.toString());
+
+        System.out.println("Getting one value of Hashmap");
+        System.out.println(examResult.get("Math"));
+        System.out.println("get function return null if key not exist in Hashmap return null");
+        System.out.println(examResult.get("English"));
+
+        System.out.println("If key not exist get default value using getOrDefault");
+        System.out.println(examResult.getOrDefault("English",-1));
+
+        System.out.println("If key not exist add value");
+        examResult.putIfAbsent("English",70);
+        System.out.println(examResult.toString());
+
+        System.out.println("Remove key from HashMap");
+        examResult.remove("English");
+        System.out.println(examResult.toString());
+
+        System.out.println("Loop through HashMap Using ForEach Loop");
+        examResult.forEach((subject,value)->{
+            examResult.replace(subject,value +5);
+        });
+        System.out.println(examResult.toString());
+
+        System.out.println("If value exist");
+        System.out.println(examResult.containsValue(85));
+        System.out.println(examResult.containsValue(100));
+
+
+        System.out.println("If key exist");
+        System.out.println(examResult.containsKey("Chemistry"));
+        System.out.println(examResult.containsKey("English"));
+
+        System.out.println("Get all values");
+        System.out.println(examResult.values());
+
+
+        System.out.println("Get all keys");
+        System.out.println(examResult.keySet());
+
+        System.out.println("Check if hashMap is empty");
+        System.out.println(examResult.isEmpty());
+
+        System.out.println("Loop through the hash map");
+        examResult.clear();
+        System.out.println(examResult.toString());
 
     }
 }
