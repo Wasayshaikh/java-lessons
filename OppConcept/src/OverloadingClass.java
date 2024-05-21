da2e3329fe0cc86f9cb779363cf0790fc077fee8
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-05-21 17:49:51 +0500

    overloading methods

---
+++ OppConcept/src/OverloadingClass.java
@@ -0,0 +1,8 @@
+public class OverloadingClass {
+    public String over_method(){
+        return "method once ";
+    }
+    public String loading(String message){
+        return message;
+    }
+}
