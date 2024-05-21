2f65ec0420e8f06dbb0bf40a07794d30a3a67a7a
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2024-05-21 17:47:50 +0500

    abstraction

---
+++ OppConcept/src/AbstractClass.java
@@ -0,0 +1,6 @@
+abstract class AbstractClass {
+    abstract void abstract_method();
+    public void working_method_in_abstract(){
+        System.out.println("Method in abstract class");
+    }
+}
---
+++ OppConcept/src/WorkAbstraction.java
@@ -0,0 +1,5 @@
+public class WorkAbstraction extends AbstractClass {
+    public void abstract_method(){
+        System.out.println("Implementing the abstract function");
+    }
+}
